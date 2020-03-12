9ce6b55f1eff2bf058d353a7369591b4435be4ae
Taz Uddin <getleton@gmail.com>
2020-03-13 01:55:58 +0600

    state is stored in Board

--- src/game/board.js
+++ src/game/board.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Square from './square';
 class Board extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill('X')            
+        }
+    }
+    handleClick(i){        
+        alert('a');
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+    
     renderSquare(i){
-        return <Square />
+        return (<Square 
+            value={this.state.squares[i]}
+            onClick={()=> this.handleClick(i)}
+            />);
     }
 
     render(){
--- src/game/square.js
+++ src/game/square.js
@@ -1,9 +1,19 @@
 import React from 'react';
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null
+        }
+        
+    }
+
+
     render(){
         return (
-            <button className="square">
-                {"X"}
+            <button className="square" 
+            onClick={()=> this.props.onClick()}>
+                {this.state.value}                
             </button>
         );
     }   
